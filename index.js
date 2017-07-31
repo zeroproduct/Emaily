8760d29ffdba4e5fee6beb6b085726356315cd44
Daniel <danrica92@yahoo.com>
2017-07-31 15:29:25 -0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ sup: 'albert' });
 });
 
 const PORT = process.env.PORT || 5000;
