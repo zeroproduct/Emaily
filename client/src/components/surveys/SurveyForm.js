d743e2e5ec4fcafc99ac93206564416e4bcb306b
Daniel <danrica92@yahoo.com>
2017-10-18 15:17:31 -0700

    Finished up Surveys form and submittion front end section. Added action creators to save survey to database.

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,32 +6,11 @@ import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
 
-const FIELDS = [
-  {
-    label: 'Survey Title',
-    name: 'title',
-    noValueError: 'You must provide a title'
-  },
-  {
-    label: 'Subject Line',
-    name: 'subject',
-    noValueError: 'You must provide a subject'
-  },
-  {
-    label: 'Email Body',
-    name: 'body',
-    noValueError: 'You must provide a body'
-  },
-  {
-    label: 'Recipient List (Separated by comma)',
-    name: 'emails',
-    noValueError: 'You must provide valid email'
-  }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -47,7 +26,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat left white-text">
             Cancel
@@ -65,9 +44,9 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  errors.emails = validateEmails(values.emails || '');
+  errors.recipients = validateEmails(values.recipients || '');
 
-  _.each(FIELDS, ({ name, noValueError }) => {
+  _.each(formFields, ({ name, noValueError }) => {
     if (!values[name]) {
       errors[name] = noValueError;
     }
@@ -78,5 +57,6 @@ function validate(values) {
 
 export default reduxForm({
   validate: validate,
-  form: 'surveyForm'
+  form: 'surveyForm',
+  destroyOnUnmount: false
 })(SurveyForm);
