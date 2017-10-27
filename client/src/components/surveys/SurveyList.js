498b44ec56daec8ed8872b82ee8b2d0641c4ece5
Daniel <danrica92@yahoo.com>
2017-10-26 18:47:51 -0700

    Created method to sort surveys list by title. Still need to refactor and maybe change into toggle

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,8 +3,19 @@ import { connect } from 'react-redux';
 import { fetchSurveys, deleteSurvey } from '../../actions';
 
 class SurveyList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      surveys: []
+    };
+  }
+
   componentDidMount() {
     this.props.fetchSurveys();
+    this.setState({
+      surveys: this.props.surveys
+    });
   }
 
   deletePost(id) {
@@ -48,9 +59,56 @@ class SurveyList extends Component {
     });
   }
 
+  sortList() {
+    const { surveys } = this.props;
+    const ordered = {};
+
+    surveys.sort((a, b) => {
+      return a.title.localeCompare(b.title);
+    });
+
+    this.setState({
+      surveys: surveys
+    });
+
+    return surveys.reverse().map(survey => {
+      return (
+        <div className="card daarken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">
+              {survey.title}
+            </span>
+            <p>
+              {survey.body}
+            </p>
+            <p className="right">
+              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a className="blue-grey-text lighten-2">
+              Yes: {survey.yes}
+            </a>
+            <a className="blue-grey-text lighten-2">
+              No: {survey.no}
+            </a>
+            <button
+              type="submit"
+              className="delete-button red darken-1 btn-flat right white-text"
+              onClick={this.deletePost.bind(this, survey._id)}
+            >
+              <i className="material-icons">delete_forever</i>
+            </button>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.sortList.bind(this)}>Sort by name</button>
         {this.renderSurveys()}
       </div>
     );
