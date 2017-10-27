137316129c381008a0e684ff596a14e20dfc23db
Daniel <danrica92@yahoo.com>
2017-10-26 19:24:16 -0700

    Refactored functions a little bit and added sort by date. Still using buttons and need to change to dropdown menu and refactor more

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,7 +7,8 @@ class SurveyList extends Component {
     super(props);
 
     this.state = {
-      surveys: []
+      surveys: [],
+      initialSurvey: true
     };
   }
 
@@ -24,92 +25,87 @@ class SurveyList extends Component {
     });
   }
 
+  renderCard(survey) {
+    return (
+      <div className="card darken-1" key={survey._id}>
+        <div className="card-content">
+          <span className="card-title">
+            {survey.title}
+          </span>
+          <p>
+            {survey.body}
+          </p>
+          <p className="right">
+            Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="card-action">
+          <a className="blue-grey-text lighten-2">
+            Yes: {survey.yes}
+          </a>
+          <a className="blue-grey-text lighten-2">
+            No: {survey.no}
+          </a>
+          <button
+            type="submit"
+            className="delete-button red darken-1 btn-flat right white-text"
+            onClick={this.deletePost.bind(this, survey._id)}
+          >
+            <i className="material-icons">delete_forever</i>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
-      return (
-        <div className="card daarken-1" key={survey._id}>
-          <div className="card-content">
-            <span className="card-title">
-              {survey.title}
-            </span>
-            <p>
-              {survey.body}
-            </p>
-            <p className="right">
-              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
-            </p>
-          </div>
-          <div className="card-action">
-            <a className="blue-grey-text lighten-2">
-              Yes: {survey.yes}
-            </a>
-            <a className="blue-grey-text lighten-2">
-              No: {survey.no}
-            </a>
-            <button
-              type="submit"
-              className="delete-button red darken-1 btn-flat right white-text"
-              onClick={this.deletePost.bind(this, survey._id)}
-            >
-              <i className="material-icons">delete_forever</i>
-            </button>
-          </div>
-        </div>
-      );
+      return this.renderCard(survey);
     });
   }
 
-  sortList() {
+  sortListByTitle() {
     const { surveys } = this.props;
-    const ordered = {};
 
     surveys.sort((a, b) => {
       return a.title.localeCompare(b.title);
     });
 
     this.setState({
-      surveys: surveys
+      surveys: surveys,
+      initialSurvey: false
     });
 
     return surveys.reverse().map(survey => {
-      return (
-        <div className="card daarken-1" key={survey._id}>
-          <div className="card-content">
-            <span className="card-title">
-              {survey.title}
-            </span>
-            <p>
-              {survey.body}
-            </p>
-            <p className="right">
-              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
-            </p>
-          </div>
-          <div className="card-action">
-            <a className="blue-grey-text lighten-2">
-              Yes: {survey.yes}
-            </a>
-            <a className="blue-grey-text lighten-2">
-              No: {survey.no}
-            </a>
-            <button
-              type="submit"
-              className="delete-button red darken-1 btn-flat right white-text"
-              onClick={this.deletePost.bind(this, survey._id)}
-            >
-              <i className="material-icons">delete_forever</i>
-            </button>
-          </div>
-        </div>
-      );
+      return this.renderCard(survey);
+    });
+  }
+
+  sortListByDate() {
+    const { surveys } = this.props;
+
+    surveys.sort((a, b) => {
+      return new Date(b.dateSent) - new Date(a.dateSent);
+    });
+
+    this.setState({
+      surveys: surveys,
+      initialSurvey: false
+    });
+
+    return surveys.reverse().map(survey => {
+      return this.renderCard(survey);
     });
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.sortList.bind(this)}>Sort by name</button>
+        Sort by:
+        <button onClick={this.sortListByDate.bind(this)}>Date</button>
+        <button onClick={this.sortListByTitle.bind(this)}>Title</button>
         {this.renderSurveys()}
+        {console.log(this.props.surveys)}
       </div>
     );
   }
